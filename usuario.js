cb200d79d683dda924eece34b0b743996ddc0c9d
Alejo-art43 <alejandro.hernandezrod23@gmail.com>
2025-10-19 22:05:52 -0500

    Update usuario.js

--- usuario.js
+++ usuario.js
@@ -1,5 +1,5 @@
-let inicio = ['juan.pizo.ueb@gmail.com']; //hacemos el arreglo unidimensional guardamos los datos asignados
-let password = ['123456789'];
+let inicio = ['usuario@gmail.com']; //hacemos el arreglo unidimensional guardamos los datos asignados
+let password = ['1234'];
 
 function cargar(){
     let usuario=document.getElementById("correo").value; //creamos variables y los llamamos desde el id del html para que tomen la funcion de esa variable
@@ -13,4 +13,5 @@ function cargar(){
     {
         alert("DATOS ERRONEOS")//mensaje de alerta cuando el usuario o persona no ingresa los datos correctos
     }
+
 }
